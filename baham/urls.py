4f75679ed85367ac717bc11862e6cc68643358d7
neerajparkashsharma <npsstudies@gmail.com>
2023-04-28 08:54:16 +0500

    implementation of database, and CRUD on vehicle

--- baham/urls.py
+++ baham/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
      path('baham/about-us',views.about,name='aboutus'),
+     path('baham/vehicles',views.view_vehicles,name='vehicles'),
+     path('baham/vehicles/create',views.create_vehicle,name='create_vehicle'),
 ]
--- baham/views.py
+++ baham/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import Vehicle
+from .enum_types import VehicleTypes
 
 # Create your views here.
 
@@ -21,4 +23,36 @@ def about(request):
     template = loader.get_template(template_name='about.html')
     return HttpResponse (template.render(context,request=request))
 
+
+
+def create_vehicle(request):
+    template = loader.get_template(template_name='createVehicle.html')
+    context = {
+    'title': 'Vehicles',
+    'heading': 'Welcome to Baham',
+    'vehicle_types': [t for t in VehicleTypes]
+    }
+
+    return HttpResponse (template.render(context,request=request))
+    
+
+
+def save_vehicle(request):
+    vendor = request.POST.get('vendor')
+    model = request.POST.get('model')
+    # vehicle_type = request.POST.get('vehicle_type')
+    vehicle = Vehicle(vendor=vendor, model=model)
+    vehicle.save()
+    return view_vehicles(request)
+
+
+def view_vehicles(request):
+    template = loader.get_template(template_name='Vehicle.html')
+    vehicles = Vehicle.objects.all().order_by('vendor')
+    context = {
+        'title': 'Vehicles',
+        'heading': 'Welcome to Baham',
+        'vehicles':  vehicles,
+    }
+    return HttpResponse (template.render(context,request=request))
 
