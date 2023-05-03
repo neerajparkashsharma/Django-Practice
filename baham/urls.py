3722eecaa87ecb3c326d11fb6f497c09fc0fab53
neerajparkashsharma <npsstudies@gmail.com>
2023-05-03 16:28:43 +0500

    model update, audit fields and delete override.

--- baham/urls.py
+++ baham/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+     path('',views.home,name='home'),
      path('baham/about-us',views.about,name='aboutus'),
      path('baham/vehicles',views.view_vehicles,name='vehicles'),
      path('baham/vehicles/create',views.create_vehicle,name='create_vehicle'),
